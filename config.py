dfe3e1dd1aafd94d4e6f3d3bbaed1c7e6a4ec09b
Siddhant Jain <siddhantjain209@gmail.com>
2024-05-31 15:00:42 +0530

    add config for proxy generator

---
+++ config.py
@@ -0,0 +1,7 @@
+PROXIES_URL = 'https://free-proxy-list.net/'
+PROXY_TEST_URL = "https://ipinfo.io/json"
+PROXY_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'
+}
+VALID_PROXIES = 'valid_proxies.txt'
+N_OF_THREADS = 8
